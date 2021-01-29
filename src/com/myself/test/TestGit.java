baecabaf4b47dde63f582b8898c11e0714dd8847
tang_global <tang_global@163.com>
2021-01-29 11:18:00 +0800

    测试ssh推送到远程

--- src/com/myself/test/TestGit.java
+++ src/com/myself/test/TestGit.java
@@ -3,7 +3,5 @@ package com.myself.test;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("测试eclipse提交代码到远程仓库");
-		System.out.println("修改了代码，再次提交");
 	}
-	
 }
