79e9b2573619592e6b9067d6e56254102296a85b
tang_global <tang_global@163.com>
2021-01-29 10:47:35 +0800

    这次测试密匙的作用

--- src/com/myself/test/TestGit.java
+++ src/com/myself/test/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("测试eclipse提交代码到远程仓库");
 		System.out.println("修改了代码，再次提交");
+		System.out.println("测试密匙");
 	}
 	
 }
